738058f930a84a21ae4594db964e6068a7bdb9fd
Madenityan <marinakorsyn@gmail.com>
2019-02-22 12:19:03 +0200

    created post request(need to fix)

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-let express = require('express');
-let fs = require('fs');
+const express = require('express');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-let app = express();
+const app = express();
 
 app.listen(3000);
 
-app.get('/todolist', function(req, res) {
-    let data =  fs.readFileSync('data.json', 'utf8');
-    res.json(JSON.parse(data));
+app.get('/todolist', function (req, res) {
+  let data = fs.readFileSync('data.json', 'utf8');
+  res.json(JSON.parse(data));
 });
 
 app.post('/create', function (req, res) {
-
+  const json = {
+    id: '5c2e8c94fb6fc02c41a54ec3',
+    userId: 'eyJhbGciOiJIUzI1NiJ9',
+    title: 'RRRRRR',
+    description: 'WWWWWW',
+    status: 'new',
+    selected: false
+  };
+  let data = fs.readFileSync('data.json', 'utf8');
+  let jsonData = JSON.parse(data);
+  jsonData.push(json);
+  console.log(json);
+  let writer = fs.writeFileSync('data.json', JSON.stringify(json),'utf-8');
+  console.log(writer);
+  res.send(json);
 });
 
